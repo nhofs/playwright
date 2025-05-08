0b19856c69e50606ebc2f5b1aab9ef7905fd3abd
Nolan <101490941+nhofs@users.noreply.github.com>
2025-05-07 23:44:24 -0400

    starting to play around

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,50 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("tests rental application inputs, errors, and submission", async ({
+  page,
+}) => {
+  const rentalAppUrl =
+    "/tenant-portal/rental-applications/listing?companyId=66c39698e06089d73242eb7d&source=CompanyLink";
+  await page.goto(rentalAppUrl);
+  await page.waitForLoadState();
+  await page.getByTestId("Listing-ListingImage").first().click();
+
+  const page1Promise = page.waitForEvent("popup");
+  await page
+    .getByTestId("Listing-ListingDetailsView-ApplyOnlineButton")
+    .click();
+  const page1 = await page1Promise;
+  await page1.getByRole("textbox", { name: "Enter your Email" }).click();
+  await page1
+    .getByRole("textbox", { name: "Enter your Email" })
+    .fill("nhofstee+" + Date.now() + "@doorloop.com");
+  await page1
+    .getByRole("checkbox", { name: "By clicking here, I confirm" })
+    .click();
+  await page1.getByRole("button", { name: "Go to the Application" }).click();
+  await expect(
+    page1.getByText("Enter the code we sent over email to")
+  ).toBeVisible();
+  await page1.pause();
+  await page1.waitForEvent("framenavigated");
+  expect(page1.getByText("Let's Get Started.")).toBeVisible();
+
+  // page
+  //   .getByRole("input", { name: "email" })
+  //   .fill("nhofstee+whatever@doorloop.com");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // await expect(page).toHaveTitle(/Doorloop/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test("get started link", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole("link", { name: "Get started" }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole("heading", { name: "Installation" })
+//   ).toBeVisible();
+// });
